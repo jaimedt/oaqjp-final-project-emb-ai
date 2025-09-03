5d85dd6cbbe05200566decfea2ba0ceb99c13c33
Jaime Torres <jaimedtorres@hotmail.com>
2025-09-03 12:43:58 -0400

    Completed task 8.

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+Uses an embedded NLP library to analyze user text and return the identified emotions
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector as ed
 
@@ -5,9 +8,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detector():
+    '''
+    Call the NLP library via http call and return the formatted response
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     results = ed(text_to_analyze)
-    if results['dominant_emotion'] == None:
+    if results['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     return f"For the given statement, the system response is 'anger': {results['anger']}, \
     'disgust': {results['disgust']}, 'fear': {results['fear']}, 'joy': {results['joy']} \
@@ -16,6 +22,9 @@ def emotion_detector():
 
 @app.route("/")
 def render_index_page():
+    '''
+    Render the web app
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
